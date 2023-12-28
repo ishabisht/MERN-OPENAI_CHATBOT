8ad5d466cc3ed537c205cb69c57cf602bdf7757e
Isha Bisht <ishabisht36@gmail.com>
2023-12-28 21:05:37 +0530

    my first

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Built  by   ISHA BISHT
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
